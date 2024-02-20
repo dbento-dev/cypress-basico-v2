17f1e069a8c6af252cb3009810b6118221a3c461
David Bento <dbento.dev@gmail.com>
2024-02-20 20:04:45 -0300

    feat: testing input and submit form data

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,9 +8,27 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-  it('verifica o título da aplicação', function () {
+  beforeEach(() => {
+    // root-level hook
+    // runs before every test block
     cy.visit('./src/index.html')
+  })
 
+  it('verifica o título da aplicação', function () {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', function () {
+    cy.get('input[id="firstName"]').type('John')
+    cy.get('input[id="lastName"]').type('Doe')
+    cy.get('input[id="email"]').type('john-doe@example.com')
+    cy.get('textarea[id="open-text-area"]').type('Lorem ipsum dolor sit amet.')
+
+    // opção para selecionar o elemento
+    // cy.get('#firstName').type('Doe')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
