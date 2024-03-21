8a57e9af29e1e9fa150495babe520dec14c3bdb0
David Bento <dbento.dev@gmail.com>
2024-03-21 20:08:06 -0300

    feat: add test using request http

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -291,11 +291,23 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .should('not.be.visible')
   })
 
-  it.only('preenche a area de texto usando o comando invoke', () => {
+  it('preenche a area de texto usando o comando invoke', () => {
     const longText = Cypress._.repeat('0123456789', 20)
 
     cy.get('#open-text-area')
       .invoke('val', longText)
       .should('have.value', longText)
+  })
+
+  it.only('faz uma requisição HTTP', () => {
+    cy.request(
+      'https://cac-tat.s3.eu-central-1.amazonaws.com/index.html',
+    ).should((resp) => {
+      const { status, statusText, body } = resp
+
+      expect(status).to.equal(200)
+      expect(statusText).to.equal('OK')
+      expect(body).to.include('CAC TAT')
+    })
   })
 })
