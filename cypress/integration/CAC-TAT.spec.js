651960f7fa3f5744daa882cb1d793b45a8d2ec29
David Bento <dbento.dev@gmail.com>
2024-03-21 19:51:41 -0300

    feat: add test using repeat and invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -290,4 +290,12 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .invoke('hide')
       .should('not.be.visible')
   })
+
+  it.only('preenche a area de texto usando o comando invoke', () => {
+    const longText = Cypress._.repeat('0123456789', 20)
+
+    cy.get('#open-text-area')
+      .invoke('val', longText)
+      .should('have.value', longText)
+  })
 })
